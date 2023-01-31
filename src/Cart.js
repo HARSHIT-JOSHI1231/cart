c79dc0bb42a2e8822ba09c1f25f3aadbcc76869c
Harshit Joshi <joshiharshit1231@gmail.com>
2023-01-31 14:32:24 +0530

    add props to cart items

--- src/Cart.js
+++ src/Cart.js
@@ -2,10 +2,46 @@ import React from 'react';
 import CartItem from './CartItem';
 
 class Cart extends React.Component {
+    constructor(){
+        super();
+        this.state ={
+            products: [
+                {
+                    price: 999,
+                    title: 'Watch',
+                    qty: 1,
+                    img:'',
+                    id:1
+                },
+                {
+                    price: 999,
+                    title: 'Mobile Phone',
+                    qty: 1,
+                    img:'',
+                    id:2
+                },
+                {
+                    price: 999,
+                    title: 'Laptop',
+                    qty: 1,
+                    img:'',
+                    id:3
+                }
+            ]
+         
+        }
+    }
     render(){
+       const {products} = this.state;
         return (
             <div className='cart'>
-                <CartItem />
+             
+              {products.map((product)=>{
+                return <CartItem 
+                product={product}
+                 key={product.id}
+                 />
+              })}
             </div>
         )
     }
--- src/CartItem.js
+++ src/CartItem.js
@@ -1,16 +1,7 @@
 import React from 'react';
 
 class CartItem extends React.Component {
-    constructor(){
-        super();
-        this.state = {
-            price: 999,
-            title: 'Mobile Phone',
-            qty: 1,
-            img: ''
-        }
-        //this.testing();
-    }
+   
 
     // testing(){
     //   const promise = new Promise((resolve, reject)=>{
@@ -53,14 +44,15 @@ class CartItem extends React.Component {
       })
     }
   render () {
-    const {price,title, qty } = this.state;
+    console.log('this.props', this.props)
+    const {price,title, qty } = this.props.product;
     return (
       <div className="cart-item">
         <div className="left-block">
           <img style={styles.image} />
         </div>
         <div className="right-block">
-          <div style={ { fontSize: 25 } }>{this.state.title}</div>
+          <div style={ { fontSize: 25 } }>{title}</div>
           <div style={ { color: '#777' } }>Rs{price}</div>
           <div style={ { color: '#777' } }>Qty: {qty}</div>
           <div className="cart-item-actions">
